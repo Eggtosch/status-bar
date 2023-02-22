10dcbbac2b9b59f7909711307fe1192d286b1a47
Oskar Munz <oskar.munz@gmx.de>
2023-02-22 23:07:22 +0100

    Change indentation from 8 spaces to tabs

--- src/block.h
+++ src/block.h
@@ -5,10 +5,10 @@
 #define BLOCK_BUFFER_SIZE 128
 
 struct block {
-        int interval;
-        void (*update)(struct block*);
+	int interval;
+	void (*update)(struct block*);
 
-        uint32_t color;
-        char text[BLOCK_BUFFER_SIZE];
-        char buffer[BLOCK_BUFFER_SIZE];
+	uint32_t color;
+	char text[BLOCK_BUFFER_SIZE];
+	char buffer[BLOCK_BUFFER_SIZE];
 };
--- src/datetime_block.c
+++ src/datetime_block.c
@@ -2,13 +2,13 @@
 #include <block.h>
 
 static void datetime_update(struct block *b) {
-        strcpy(b->text, "Hello world");
+	strcpy(b->text, "Hello world");
 }
 
 struct block datetime_block_init(void) {
-        struct block b;
-        b.interval = 2;
-        b.update = datetime_update;
-        b.color = 0xffffff;
-        return b;
+	struct block b;
+	b.interval = 2;
+	b.update = datetime_update;
+	b.color = 0xffffff;
+	return b;
 }
