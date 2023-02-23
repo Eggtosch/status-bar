aed696f717a871fdcb8f2faaf1bee65cb029a800
Oskar Munz <oskar.munz@gmx.de>
2023-02-23 01:58:51 +0100

    Port sound_block to pactl

--- src/sound_block.c
+++ src/sound_block.c
@@ -4,29 +4,44 @@
 
 #include <block.h>
 
+static char buf[4096];
+
 static void sound_update(struct block *b) {
-	FILE *f = popen("amixer -M get Master", "r");
-
-	while (fgetc(f) != '[');
-
-	int volume = 0;
-	int c;
-	while ((c = fgetc(f)) != '%') {
-		volume *= 10;
-		volume += c - '0';
+	FILE *f = popen("pactl list sinks", "r");
+	if (f == NULL) {
+		return;
 	}
 
-	char line[256];
-	fgets(line, sizeof(line), f);
-	if (strstr(line, "on") != NULL) {
-		b->color = 0xffffff;
-	} else if (strstr(line, "off") != NULL) {
+	fread(buf, sizeof(buf), 1, f);
+
+	pclose(f);
+
+	if (strstr(buf, "Mute: yes") != NULL) {
 		b->color = 0xffff00;
+	} else if (strstr(buf, "Mute: no") != NULL) {
+		b->color = 0xffffff;
 	} else {
 		b->color = 0xff0000;
 	}
 
-	pclose(f);
+	char *volume_line = strstr(buf, "Volume:");
+	if (volume_line == NULL) {
+		return;
+	}
+
+	char *percentage = strchr(volume_line, '%');
+	if (percentage == NULL) {
+		return;
+	}
+	percentage--;
+
+	int volume = 0;
+	int multiplier = 1;
+	while (*percentage >= '0' && *percentage <= '9') {
+		volume += (*percentage - '0') * multiplier;
+		multiplier *= 10;
+		percentage--;
+	}
 
 	snprintf(b->text, BLOCK_BUFFER_SIZE, "%d%%", volume);
 }
